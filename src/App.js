2b4dfdb60c57e17d171ce7caabb92eb4dc49e553
Khushi <khushimuskaan1999@gmail.com>
2021-12-09 14:13:08 +0530

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import './App.css';
-import Sidebar from './component/Sidebar';
-import Hlayout from './component/Hlayout' ;  
-import Modal from './component/Modal' ; 
-import Vlayout from './component/Vlayout' ;
-import { Route , Routes}from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useNavigate, useLocation} from "react-router-dom";
+import "./App.css";
+import Sidebar from "./component/Sidebar";
+import Hlayout from "./component/Hlayout";
+import Modal from "./component/Modal";
+import Vlayout from "./component/Vlayout";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 function App() {
-  const [showModal,setShowModal]=useState(false);
-  const location=useLocation();
+  const [showModal, setShowModal] = useState(false);
+  const location = useLocation();
   const navigate = useNavigate();
 
-  
-  useEffect(()=>{
-    if(location.pathname==="/"){
-      navigate('/Hlayout');
+  useEffect(() => {
+    if (location.pathname === "/reacttask/") {
+      navigate("/Hlayout");
     }
-  },[])
+  }, []);
   return (
     <>
-    <div className="App">
-     <Sidebar/>
-     <Routes>
-       <Route path='/Hlayout'  element={<Hlayout setShowModal={setShowModal}/>} />
-       <Route path='/Vlayout' element={<Vlayout setShowModal={setShowModal}/>} />
-    </Routes>  
-    </div>   
-   <div className={`componets ${showModal?"show-modal":""}`}> 
-    <Modal setShowModal={setShowModal}/>
-    </div>
+      <div className="App">
+        <Sidebar />
+        <Routes>
+          <Route
+            path="/Hlayout"
+            element={<Hlayout setShowModal={setShowModal} />}
+          />
+          <Route
+            path="/Vlayout"
+            element={<Vlayout setShowModal={setShowModal} />}
+          />
+        </Routes>
+      </div>
+      <div className={`componets ${showModal ? "show-modal" : ""}`}>
+        <Modal setShowModal={setShowModal} />
+      </div>
     </>
   );
 }
